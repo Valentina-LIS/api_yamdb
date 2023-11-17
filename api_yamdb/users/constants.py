d09a4da64a6515cdb6ffff20da11c8f4e21df93d
AlexanderGrabenko <axelgrauberg@gmail.com>
2023-11-17 01:46:55 +0100

    Приложение `users`
    - Реализованы сериализаторы

--- api_yamdb/users/constants.py
+++ api_yamdb/users/constants.py
@@ -5,3 +5,7 @@ USER = 'user'
 
 # constants
 FIELD_DEFAULT_LEN = 150
+CONFIRM_CODE_SIZE = 6
+NO_REPLY_MAIL = 'api_yamdb@example.com'
+USERNAME_REGEX = r'^[\w.@+-]+$'
+PROHIBITED_USERNAMES = ('me', 'admin')
