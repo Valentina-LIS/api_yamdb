c554457891daf73b5bac67ee9ab4de464f3a250b
AlexanderGrabenko <axelgrauberg@gmail.com>
2023-11-19 16:35:04 +0100

    Приложение `reviews`
    - Регистрация админ-панели перенесена из `users` в `reviews`

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
 from users.models import CustomUser
-from reviews.models import Genre, Category, Title
 
 admin.site.register(CustomUser)
-admin.site.register(Genre)
-admin.site.register(Category)
-admin.site.register(Title)
