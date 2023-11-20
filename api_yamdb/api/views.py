6ffef3239ac191624c4ae5838fc6e7456f72e92c
Валентина Лисина <lisinavalentina97@gmail.com>
2023-11-20 03:25:33 +0300

    Сhange views.py, admin.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,10 +2,11 @@ from rest_framework import filters, viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
+
 from reviews.models import Category, Genre, Title, Review
-from rest_framework import permissions
 from api.filters import TitleFilter
-from users.permissions import IsAdminOrReadOnly
+from users.permissions import (IsAdminOrReadOnly,
+                               IsAuthorModeratorAdminOrReadOnly)
 from api.serializers import (CategorySerializer, CommentsSerializer,
                              GenreSerializer, ReviewsSerializer,
                              TitleSerializer, TitleCreateSerializer)
@@ -51,30 +52,30 @@ class GenreViewSet(viewsets.ModelViewSet):
 class ReviewsViewSet(viewsets.ModelViewSet):
     """Отзывы."""
     serializer_class = ReviewsSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAdmin)
+    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
-
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
         return title.reviews.all()
 
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
+        title = get_object_or_404(Title, pk=title_id)
         serializer.save(author=self.request.user, title=title)
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
     """Комментарии."""
     serializer_class = CommentsSerializer
-    permission_classes = (permissions.IsAuthenticated, IsAdmin)
+    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id)
         return review.comments.all()
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id, title=title_id)
+        review = get_object_or_404(Review, pk=review_id)
         serializer.save(author=self.request.user, review=review)
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Genre, GenreTitle, Review, Title, User
+from .models import Category, Comment, Genre, GenreTitle, Review, Title
 
-admin.site.register(User)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Genre)
